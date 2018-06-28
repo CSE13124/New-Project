f660c0ffa2cd4c7f08893ec43e0037074c722f02
CSE13124 <mjohail95@gmail.com>
2018-06-28 10:40:46 +0530

    Hey I changed the program

--- src/HelloWorld/HelloExample.java
+++ src/HelloWorld/HelloExample.java
@@ -1,15 +1,10 @@
 package HelloWorld;
 
-import java.util.Scanner;
 
 public class HelloExample {
 	
 	public static void main(String[] args){
-		System.out.println("Enter any number:");
-		Scanner scan = new Scanner(System.in);
-		int i= scan.nextInt();
-		int a =i+5;
-		System.out.println("The sum of "+ i +" + 5: "+ a);
+		System.out.println("Hello World!");
 	}
 
 }
